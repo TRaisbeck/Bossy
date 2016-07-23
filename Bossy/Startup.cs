371292749368ed9b7e1177a9a5338322a2d16eb3
Tom <Tom@WILMA>
2016-07-23 14:36:57 -0400

    Add project files.

---
+++ Bossy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bossy.Startup))]
+namespace Bossy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
